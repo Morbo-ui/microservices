b2f1ded8b981275809cce60b9ae6da9880abcded
Dmitry Buzin <buzindmitry.dev@gmail.com>
2024-05-17 02:23:34 +0300

    first commit

--- customer-service/src/main/java/com/dmitrii/CustomerApplication.java
+++ customer-service/src/main/java/com/dmitrii/CustomerApplication.java
@@ -1,13 +1,13 @@
 package com.dmitrii;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApplication
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(CustomerApplication.class, args);
     }
 }
