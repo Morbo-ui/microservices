5bfcf52a013e54bc5652784a680b719a384eab6f
Dmitry Buzin <buzindmitry.dev@gmail.com>
2024-05-17 17:57:56 +0300

    add Eureka + LoadBalanced

--- customer-service/src/main/java/com/dmitrii/Customer.java
+++ customer-service/src/main/java/com/dmitrii/Customer.java
@@ -1,15 +1,16 @@
 package com.dmitrii;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 
 @Data
--- fraud-service/src/main/java/com/dmitrii/fraud/FraudCheckHistory.java
+++ fraud-service/src/main/java/com/dmitrii/fraud/FraudCheckHistory.java
@@ -1,15 +1,15 @@
 package com.dmitrii.fraud;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import java.time.LocalDateTime;
 
 @Data
