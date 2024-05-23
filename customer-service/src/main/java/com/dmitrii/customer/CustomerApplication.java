007a1d1fc7f566d18a6c5ecc76bdc6b30676837b
Dmitry Buzin <buzindmitry.dev@gmail.com>
2024-05-23 19:43:54 +0300

    add k8s + refactoring

--- customer-service/src/main/java/com/dmitrii/customer/CustomerApplication.java
+++ customer-service/src/main/java/com/dmitrii/customer/CustomerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -11,10 +13,13 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
                 "com.dmitrii.amqp",
         }
 )
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableFeignClients(
         basePackages = "com.dmitrii.clients"
 )
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class CustomerApplication
 {
     public static void main(String[] args)
--- notification-service/src/main/java/com/dmitrii/notifications/NotificationsApplication.java
+++ notification-service/src/main/java/com/dmitrii/notifications/NotificationsApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
 		scanBasePackages = {
@@ -12,7 +14,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 		}
 )
 @EnableFeignClients
-@EnableEurekaClient
+//@EnableEurekaClient
+@PropertySources({
+		@PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class NotificationsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationsApplication.class, args);
